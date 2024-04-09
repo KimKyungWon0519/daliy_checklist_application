7f515f75e4cd49d84705b68e4d3c390d85d92fac
KimKyungWon0519 <gwon080870@gmail.com>
2024-04-10 02:12:18 +0900

    FEAT: Add home route

--- lib/src/core/routes/app_pages.dart
+++ lib/src/core/routes/app_pages.dart
@@ -1,3 +1,14 @@
+import 'package:go_router/go_router.dart';
+
+import 'app_routes.dart';
+
 final class AppPages {
   const AppPages._();
+
+  static final String _initialLocation = AppRoutes.home.path;
+
+  static final GoRouter routeConfig = GoRouter(
+    routes: AppRoutes.routes,
+    initialLocation: _initialLocation,
+  );
 }
--- lib/src/core/routes/app_routes.dart
+++ lib/src/core/routes/app_routes.dart
@@ -1,3 +1,15 @@
+import 'package:daily_checklist_application/src/features/home/presentation/pages/home_page.dart';
+import 'package:go_router/go_router.dart';
+
 final class AppRoutes {
   const AppRoutes._();
+
+  static final GoRoute home = GoRoute(
+    path: '/home',
+    builder: (context, state) => const HomePage(),
+  );
+
+  static final List<GoRoute> routes = [
+    home,
+  ];
 }
