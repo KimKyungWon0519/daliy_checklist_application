47ba4fc92ee3de1f185c7ad56232fc618e3d5ca9
KimKyungWon0519 <gwon080870@gmail.com>
2024-04-13 22:27:42 +0900

    FEAT: Delete snapping scroll pyhsics

--- lib/src/features/home/presentation/pages/home_page/physics/snapping_scroll_physics.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-
-class SnappingScrollPhysics extends ScrollPhysics {
-  final double itemExtent;
-
-  const SnappingScrollPhysics({required this.itemExtent, super.parent});
-}
