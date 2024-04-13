0320b75a3fbe43b3c92d9f848987365a3fca59dc
KimKyungWon0519 <gwon080870@gmail.com>
2024-04-13 22:18:15 +0900

    FEAT: Change class name

--- lib/src/features/home/presentation/pages/home_page/physics/snapping_scroll_physics.dart
+++ lib/src/features/home/presentation/pages/home_page/physics/snapping_scroll_physics.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-class SnappingScrollPysics extends ScrollPhysics {
+class SnappingScrollPhysics extends ScrollPhysics {
   final double itemExtent;
 
-  const SnappingScrollPysics({required this.itemExtent, super.parent});
+  const SnappingScrollPhysics({required this.itemExtent, super.parent});
 }
