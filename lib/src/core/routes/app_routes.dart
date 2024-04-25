f272c1776510aa151cd708fee2fa18f889182a2e
KimKyungWon0519 <gwon080870@gmail.com>
2024-04-26 00:49:45 +0900

    FEAT: Change route value

--- lib/src/core/routes/app_routes.dart
+++ lib/src/core/routes/app_routes.dart
@@ -4,11 +4,6 @@ import 'package:home_feature/home_feature.dart';
 final class AppRoutes {
   const AppRoutes._();
 
-  static final GoRoute home = GoRoute(
-    path: '/home',
-    builder: (context, state) => const HomePage(),
-  );
-
   static final List<GoRoute> routes = [
     home,
   ];
