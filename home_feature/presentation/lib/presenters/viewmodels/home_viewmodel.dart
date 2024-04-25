01dc2af496e0eba1656c399a0ee1a7f4d373ade4
KimKyungWon0519 <gwon080870@gmail.com>
2024-04-25 15:38:57 +0900

    FEAT: Add dateTimeProvider

--- home_feature/presentation/lib/presenters/viewmodels/home_viewmodel.dart
+++ home_feature/presentation/lib/presenters/viewmodels/home_viewmodel.dart
@@ -1,1 +1,9 @@
-class HomeViewModel {}
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class HomeViewModel {
+  late final StateProvider<DateTime> currentDateProvider;
+
+  HomeViewModel() {
+    currentDateProvider = StateProvider((ref) => DateTime.now());
+  }
+}
