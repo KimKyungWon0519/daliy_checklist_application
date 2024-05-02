4d5dbe9aa4522ed2eb34a940159c580415ee7436
KimKyungWon0519 <gwon080870@gmail.com>
2024-05-02 17:52:11 +0900

    FEAT: Create task model

---
+++ home_feature/domain/lib/model/task.dart
@@ -0,0 +1,23 @@
+import 'package:domain/domain.dart';
+
+class Task {
+  final String goal;
+  final SelectedDate selectedDate;
+
+  const Task({
+    required this.goal,
+    required this.selectedDate,
+  });
+
+  factory Task.empty() => Task(
+        goal: '',
+        selectedDate: SelectedDate.empty(),
+      );
+
+  Task copyWith({String? goal, SelectedDate? selectedDate}) {
+    return Task(
+      goal: goal ?? this.goal,
+      selectedDate: selectedDate ?? this.selectedDate,
+    );
+  }
+}
