c32e24b75eda68f2b12d1714df08fd1a485f6084
KimKyungWon0519 <gwon080870@gmail.com>
2024-05-09 00:42:28 +0900

    FEAT: Create task file

---
+++ home_feature/data/lib/entites/task.dart
@@ -0,0 +1,18 @@
+import 'package:isar/isar.dart';
+
+part 'task.g.dart';
+
+@collection
+class Task {
+  final Id id = Isar.autoIncrement;
+
+  final String goal;
+  final DateTime startDate;
+  final DateTime? endDate;
+
+  const Task({
+    required this.goal,
+    required this.startDate,
+    required this.endDate,
+  });
+}
