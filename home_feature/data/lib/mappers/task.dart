17519172f144f2a690517ea72d66a3c281c29a44
KimKyungWon0519 <gwon080870@gmail.com>
2024-05-11 18:24:20 +0900

    FEAT: Create M2E extension on task

---
+++ home_feature/data/lib/mappers/task.dart
@@ -0,0 +1,12 @@
+import 'package:data/entites/task.dart' as Entity;
+import 'package:domain/domain.dart' as Model;
+
+extension TaskMapper on Model.Task {
+  Entity.Task toEntity() {
+    return Entity.Task(
+      goal: goal,
+      startDate: selectedDate.startDate,
+      endDate: selectedDate.endDate,
+    );
+  }
+}
