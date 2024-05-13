9b3a399895aa6fa80bf220fece9b67b106ad8aaa
KimKyungWon0519 <gwon080870@gmail.com>
2024-05-13 14:44:57 +0900

    FEAT: Change extension name

--- home_feature/data/lib/mappers/task.dart
+++ home_feature/data/lib/mappers/task.dart
@@ -1,7 +1,7 @@
 import 'package:data/entites/task.dart' as Entity;
 import 'package:domain/domain.dart' as Model;
 
-extension TaskMapper on Model.Task {
+extension DomainTaskMapper on Model.Task {
   Entity.Task toEntity() {
     return Entity.Task(
       goal: goal,
