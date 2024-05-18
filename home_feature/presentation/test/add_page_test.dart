f8334fec76524cfb27787494d4b63db4a34dd0e7
KimKyungWon0519 <gwon080870@gmail.com>
2024-05-18 20:21:47 +0900

    TEST: Create MockAddViewModel

--- home_feature/presentation/test/add_page_test.dart
+++ home_feature/presentation/test/add_page_test.dart
@@ -1,6 +1,8 @@
+import 'package:domain/domain.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
 import 'package:presentation/constants/app_constants.dart';
 import 'package:presentation/pages/add_task_page/add_task_page.dart';
 import 'package:presentation/pages/add_task_page/local_widgets/date_field.dart';
@@ -8,8 +10,16 @@ import 'package:presentation/pages/add_task_page/local_widgets/date_range_type_c
 import 'package:presentation/pages/add_task_page/local_widgets/goal_field.dart';
 import 'package:presentation/presenters/viewmodels/add_viewmodel.dart';
 
+class MockAddViewModel extends Mock implements AddViewModel {
+  @override
+  final StateProvider<DateType> dateTypeProvider =
+      StateProvider((ref) => DateType.daily);
+  @override
+  final StateProvider<Task> taskProvider = StateProvider((ref) => Task.empty());
+}
+
 void main() {
-  viewModelProvider.registerFactory(() => AddViewModel());
+  viewModelProvider.registerFactory<AddViewModel>(() => MockAddViewModel());
   testWidgets('test goal input widget', (widgetTester) async {
     await widgetTester.pumpWidget(
       ProviderScope(
