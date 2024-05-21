1e32a4b19900561d5bcc93ea94cf762a54c48b5b
KimKyungWon0519 <gwon080870@gmail.com>
2024-05-21 15:11:54 +0900

    FEAT: Create tasks provider

--- home_feature/presentation/lib/presenters/viewmodels/home_viewmodel.dart
+++ home_feature/presentation/lib/presenters/viewmodels/home_viewmodel.dart
@@ -1,9 +1,12 @@
+import 'package:domain/domain.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class HomeViewModel {
   late final StateProvider<DateTime> selectedDateProvider;
+  late final StateProvider<List<Task>> tasksProvider;
 
   HomeViewModel() {
     selectedDateProvider = StateProvider((ref) => DateTime.now());
+    tasksProvider = StateProvider((ref) => []);
   }
 }
