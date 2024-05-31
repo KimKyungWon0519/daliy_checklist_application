526ebfb6d2e4351abf3d2f86e1929e0d8bb8ac13
KimKyungWon0519 <gwon080870@gmail.com>
2024-05-31 19:17:19 +0900

    FEAT: Create hsl color provider

--- home_feature/presentation/lib/pages/add_task_page/local_widgets/color_picker_dialog.dart
+++ home_feature/presentation/lib/pages/add_task_page/local_widgets/color_picker_dialog.dart
@@ -1,6 +1,42 @@
 import 'package:flutter/material.dart';
 import 'package:presentation/pages/add_task_page/decorations/slider_track_shapes.dart';
 
+class _HSLColorProvider extends InheritedWidget {
+  final double hue;
+  final double saturation;
+  final double lightness;
+  final void Function({double? hue, double? saturation, double? lightness})
+      updateHSL;
+
+  const _HSLColorProvider({
+    required super.child,
+    required this.hue,
+    required this.saturation,
+    required this.lightness,
+    required this.updateHSL,
+  });
+
+  static _HSLColorProvider? maybeOf(final BuildContext context) {
+    return context.dependOnInheritedWidgetOfExactType<_HSLColorProvider>();
+  }
+
+  static _HSLColorProvider of(final BuildContext context) {
+    final _HSLColorProvider? result = maybeOf(context);
+
+    assert(result != null, 'No CalendarProvider found in context');
+
+    return result!;
+  }
+
+  @override
+  bool updateShouldNotify(covariant _HSLColorProvider oldWidget) {
+    return true;
+  }
+
+  Color get hslColor =>
+      HSLColor.fromAHSL(1, hue, saturation, lightness).toColor();
+}
+
 class ColorPickerDialog extends StatefulWidget {
   final Color color;
 
@@ -16,6 +52,7 @@ class ColorPickerDialog extends StatefulWidget {
 class _ColorPickerDialogState extends State<ColorPickerDialog> {
   double _hue = 0;
   double _lightness = 0.5;
+  double _saturation = 1;
 
   @override
   void initState() {
@@ -24,96 +61,95 @@ class _ColorPickerDialogState extends State<ColorPickerDialog> {
     HSLColor hslColor = HSLColor.fromColor(widget.color);
     _hue = hslColor.hue;
     _lightness = hslColor.lightness;
+    _saturation = hslColor.saturation;
   }
 
   @override
   Widget build(BuildContext context) {
-    Color hueColor = HSLColor.fromAHSL(1, _hue, 1, 0.5).toColor();
-    Color mainColor = HSLColor.fromAHSL(1, _hue, 1, _lightness).toColor();
+    Color mainColor =
+        HSLColor.fromAHSL(1, _hue, _saturation, _lightness).toColor();
 
-    return Dialog(
-      clipBehavior: Clip.hardEdge,
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          Container(
-            color: mainColor,
-            height: 100,
-          ),
-          Padding(
-            padding: const EdgeInsets.all(10),
-            child: Column(
-              children: [
-                _HueSlider(
-                  value: _hue,
-                  hueColor: hueColor,
-                  onChanged: (value) {
-                    setState(() {
-                      _hue = value;
-                    });
-                  },
-                ),
-                _LightnessSlider(
-                  lightness: _lightness,
-                  hueColor: hueColor,
-                  mainColor: mainColor,
-                  onChanged: (value) {
-                    setState(() {
-                      _lightness = value;
-                    });
-                  },
-                ),
-              ],
+    return _HSLColorProvider(
+      hue: _hue,
+      saturation: _saturation,
+      lightness: _lightness,
+      updateHSL: ({hue, lightness, saturation}) {
+        setState(() {
+          _hue = hue ?? _hue;
+          _saturation = saturation ?? _saturation;
+          _lightness = lightness ?? _lightness;
+        });
+      },
+      child: Dialog(
+        clipBehavior: Clip.hardEdge,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Container(
+              color: mainColor,
+              height: 100,
             ),
-          ),
-          Align(
-            alignment: Alignment.centerRight,
-            child: Padding(
-              padding: const EdgeInsets.all(5),
-              child: _SaveButton(
-                onPressed: () {
-                  Navigator.pop(context, mainColor);
-                },
+            const Padding(
+              padding: EdgeInsets.all(10),
+              child: _SliderPanel(),
+            ),
+            const Align(
+              alignment: Alignment.centerRight,
+              child: Padding(
+                padding: EdgeInsets.all(5),
+                child: _SaveButton(),
               ),
-            ),
-          )
-        ],
+            )
+          ],
+        ),
       ),
     );
   }
 }
 
 class _SaveButton extends StatelessWidget {
-  final VoidCallback? onPressed;
-
   const _SaveButton({
     super.key,
-    this.onPressed,
   });
 
   @override
   Widget build(BuildContext context) {
     return TextButton(
-      onPressed: onPressed,
-      child: Text('저장'),
+      onPressed: () {
+        final _HSLColorProvider provider = _HSLColorProvider.of(context);
+
+        Navigator.pop(context, provider.hslColor);
+      },
+      child: const Text('저장'),
+    );
+  }
+}
+
+class _SliderPanel extends StatelessWidget {
+  const _SliderPanel({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+      children: [
+        _HueSlider(),
+        _SaturationSlider(),
+        _LightnessSlider(),
+      ],
     );
   }
 }
 
 class _HueSlider extends StatelessWidget {
-  final double value;
-  final ValueChanged<double>? onChanged;
-  final Color hueColor;
-
   const _HueSlider({
     super.key,
-    required this.value,
-    required this.hueColor,
-    this.onChanged,
   });
 
   @override
   Widget build(BuildContext context) {
+    final _HSLColorProvider provider = _HSLColorProvider.of(context);
+    final double hue = provider.hue;
+
     return _ColorSlider(
       name: 'H',
       colors: const [
@@ -126,40 +162,65 @@ class _HueSlider extends StatelessWidget {
         Colors.purple,
         Colors.red,
       ],
-      value: value,
-      thumbColor: hueColor,
+      value: hue,
+      thumbColor: HSLColor.fromAHSL(1, hue, 1, 0.5).toColor(),
       max: 360,
-      onChanged: onChanged,
+      onChanged: (value) {
+        provider.updateHSL(hue: value);
+      },
     );
   }
 }
 
 class _LightnessSlider extends StatelessWidget {
-  final double lightness;
-  final Color hueColor;
-  final Color mainColor;
-  final ValueChanged<double>? onChanged;
-
   const _LightnessSlider({
     super.key,
-    required this.lightness,
-    required this.hueColor,
-    required this.mainColor,
-    this.onChanged,
   });
 
   @override
   Widget build(BuildContext context) {
+    final _HSLColorProvider provider = _HSLColorProvider.of(context);
+    final double hue = provider.hue;
+    final double saturation = provider.saturation;
+
     return _ColorSlider(
       name: 'L',
       colors: [
         Colors.black,
-        hueColor,
+        HSLColor.fromAHSL(1, hue, saturation, 0.5).toColor(),
         Colors.white,
       ],
-      value: lightness,
-      thumbColor: mainColor,
-      onChanged: onChanged,
+      value: provider.lightness,
+      thumbColor: provider.hslColor,
+      onChanged: (value) {
+        provider.updateHSL(lightness: value);
+      },
+    );
+  }
+}
+
+class _SaturationSlider extends StatelessWidget {
+  const _SaturationSlider({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final _HSLColorProvider provider = _HSLColorProvider.of(context);
+    final double hue = provider.hue;
+    final double saturation = provider.saturation;
+
+    return _ColorSlider(
+      name: 'S',
+      colors: [
+        HSLColor.fromAHSL(1, hue, 0, 0.5).toColor(),
+        HSLColor.fromAHSL(1, hue, 1, 0.5).toColor()
+      ],
+      value: saturation,
+      thumbColor: HSLColor.fromAHSL(1, hue, saturation, 0.5).toColor(),
+      onChanged: (value) {
+        provider.updateHSL(saturation: value);
+      },
     );
   }
 }
