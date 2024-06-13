3e1aedde06dd648c1cc6a0f389f330b6f7ad86ab
KimKyungWon0519 <gwon080870@gmail.com>
2024-06-13 21:51:19 +0900

    FEAT: Export home page

--- home_feature/presentation/lib/presentation.dart
+++ home_feature/presentation/lib/presentation.dart
@@ -1,7 +1,1 @@
-library presentation;
-
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export 'package:presentation/pages/home_page/home_page.dart';
