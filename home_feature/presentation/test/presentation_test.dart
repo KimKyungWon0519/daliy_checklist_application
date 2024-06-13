8cd3e78dc4933888a43ebf005a73edea4266ce15
KimKyungWon0519 <gwon080870@gmail.com>
2024-06-13 22:54:44 +0900

    TEST: Test for home page

--- home_feature/presentation/test/presentation_test.dart
+++ home_feature/presentation/test/presentation_test.dart
@@ -1,12 +1,68 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
+import 'package:presentation/pages/home_page/local_widgets/task_info_card.dart';
+import 'package:presentation/pages/home_page/local_widgets/task_info_widgets.dart';
 import 'package:presentation/presentation.dart';
 
 void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
+  group('test home page', () {
+    testWidgets('test for task card ( show date )', (widgetTester) async {
+      await widgetTester.pumpWidget(
+        const MaterialApp(
+          home: Scaffold(
+            body: TaskInfoCard(title: 'test', date: 'test', count: 0),
+          ),
+        ),
+      );
+
+      expect(find.byType(Row), findsOneWidget);
+
+      Row row = widgetTester.widget(find.byType(Row));
+
+      expect(row.children.first, isInstanceOf<Column>());
+      expect(row.children.last, isInstanceOf<Text>());
+
+      Column column = row.children.first as Column;
+
+      expect(column.children.length, 2);
+
+      expect(column.children.first, isInstanceOf<Text>());
+      expect(column.children.last, isInstanceOf<Text>());
+    });
+
+    testWidgets('test for task card ( unshow date )', (widgetTester) async {
+      await widgetTester.pumpWidget(
+        const MaterialApp(
+          home: Scaffold(
+            body: TaskInfoCard(title: 'test', count: 0),
+          ),
+        ),
+      );
+
+      expect(find.byType(Row), findsOneWidget);
+
+      Row row = widgetTester.widget(find.byType(Row));
+
+      expect(row.children.first, isInstanceOf<Column>());
+      expect(row.children.last, isInstanceOf<Text>());
+
+      Column column = row.children.first as Column;
+
+      expect(column.children.length, 1);
+
+      expect(column.children.first, isInstanceOf<Text>());
+    });
+
+    testWidgets('home page', (widgetTester) async {
+      await widgetTester.pumpWidget(
+        const MaterialApp(
+          home: HomePage(),
+        ),
+      );
+
+      expect(find.byType(TodayInfo), findsOneWidget);
+      expect(find.byType(PostponeInfo), findsOneWidget);
+      expect(find.byType(FutureInfo), findsOneWidget);
+    });
   });
 }
