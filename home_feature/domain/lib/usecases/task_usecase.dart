c5786d9501c059bd60feedb084ab1c7e9756d823
KimKyungWon0519 <gwon080870@gmail.com>
2024-06-16 17:41:03 +0900

    FEAT: Create TaskUseCase

---
+++ home_feature/domain/lib/usecases/task_usecase.dart
@@ -0,0 +1,18 @@
+import 'package:domain/repositories/task_repository.dart';
+import 'package:shared_domain/shared_domain.dart';
+
+class GetTask {
+  late final TaskRepository _taskRepository;
+
+  GetTask({
+    required TaskRepository taskRepository,
+  }) : _taskRepository = taskRepository;
+
+  Future<List<Task>> getAllTasks() {
+    return _taskRepository.getAllTasks();
+  }
+
+  Stream<void> getTasksChangedWatcher() {
+    return _taskRepository.getTasksChangedWatcher();
+  }
+}
