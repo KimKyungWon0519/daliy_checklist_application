de45639195239279aff3406b6aabbf29cda85d07
KimKyungWon0519 <gwon080870@gmail.com>
2024-06-18 16:16:11 +0900

    FEAT: Listen Task Update

--- home_feature/presentation/lib/presenter/viewmodels/home_viewmodel.dart
+++ home_feature/presentation/lib/presenter/viewmodels/home_viewmodel.dart
@@ -1,1 +1,15 @@
-class HomeViewModel {}
+import 'package:domain/domain.dart';
+
+class HomeViewModel {
+  late final GetTask _getTask;
+
+  HomeViewModel({
+    required GetTask getTask,
+  }) : _getTask = getTask {
+    _listenTaskUpdate();
+  }
+
+  void _listenTaskUpdate() {
+    _getTask.getTasksChangedWatcher().listen((event) {});
+  }
+}
