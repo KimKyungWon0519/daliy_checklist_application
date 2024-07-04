5b799f9779d3047feaa7790c0894057d3398874b
KimKyungWon0519 <gwon080870@gmail.com>
2024-07-04 17:31:40 +0900

    FEAT: Get Title, Tasks Parameter on Detail Page

--- home_feature/presentation/lib/pages/detail_page/detail_page.dart
+++ home_feature/presentation/lib/pages/detail_page/detail_page.dart
@@ -1,13 +1,21 @@
+import 'package:domain/domain.dart';
 import 'package:flutter/material.dart';
 import 'package:presentation/pages/detail_page/local_widgets/daily_tile.dart';
 
 class DetailPage extends StatelessWidget {
-  const DetailPage({super.key});
+  final String title;
+  final List<Task> tasks;
+
+  const DetailPage({
+    super.key,
+    required this.title,
+    required this.tasks,
+  });
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(title: Text(title)),
       body: Padding(
         padding: const EdgeInsets.all(8),
         child: ListView.builder(
