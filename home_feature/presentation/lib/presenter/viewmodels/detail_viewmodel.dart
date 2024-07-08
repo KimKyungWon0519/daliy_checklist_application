95ec2878424e273446abf48ce3aacfacfaa984cd
KimKyungWon0519 <gwon080870@gmail.com>
2024-07-08 16:05:28 +0900

    FEAT: Add TasksProvider

--- home_feature/presentation/lib/presenter/viewmodels/detail_viewmodel.dart
+++ home_feature/presentation/lib/presenter/viewmodels/detail_viewmodel.dart
@@ -1,1 +1,8 @@
-class DetailViewModel {}
+import 'package:domain/domain.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class DetailViewModel {
+  late final StateProvider<List<Task>> tasksProvider;
+
+  DetailViewModel() : tasksProvider = StateProvider((ref) => []);
+}
