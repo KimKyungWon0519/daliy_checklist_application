81eacf06c67ed00892f9791d2e359a14ecf3965d
KimKyungWon0519 <gwon080870@gmail.com>
2024-07-08 15:53:51 +0900

    FEAT: Create TasksType

--- home_feature/domain/lib/domain.dart
+++ home_feature/domain/lib/domain.dart
@@ -3,3 +3,4 @@ export 'package:shared_domain/domain.dart';
 export 'package:domain/usecases/task_usecase.dart';
 export 'package:domain/repositories/task_handler_repository.dart';
 export 'package:domain/usecases/task_handler.dart';
+export 'package:domain/models/tasks_type.dart';
---
+++ home_feature/domain/lib/models/tasks_type.dart
@@ -0,0 +1,9 @@
+enum TasksType {
+  today('오늘 할 일'),
+  future('앞으로 할 일'),
+  postpone('지난 할 일'),
+  all('모든 할 일'),
+  completed('완료된 일');
+
+  const TasksType(String name);
+}
