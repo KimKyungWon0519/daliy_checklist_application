17b764af768b71f96aee0e8071adb528b5cdbff7
KimKyungWon0519 <gwon080870@gmail.com>
2024-07-08 16:07:44 +0900

    FEAT: Change stl => ConsumerStf on DetailPage

--- home_feature/presentation/lib/pages/detail_page/detail_page.dart
+++ home_feature/presentation/lib/pages/detail_page/detail_page.dart
@@ -2,9 +2,10 @@ import 'dart:collection';
 
 import 'package:domain/domain.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:presentation/pages/detail_page/local_widgets/daily_tile.dart';
 
-class DetailPage extends StatelessWidget {
+class DetailPage extends ConsumerStatefulWidget {
   final String title;
   final List<Task> tasks;
 
@@ -15,9 +16,14 @@ class DetailPage extends StatelessWidget {
   });
 
   @override
+  ConsumerState<DetailPage> createState() => _DetailPageState();
+}
+
+class _DetailPageState extends ConsumerState<DetailPage> {
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text(title)),
+      appBar: AppBar(title: Text(widget.title)),
       body: Padding(
         padding: const EdgeInsets.all(8),
         child: ListView.builder(
@@ -39,7 +45,7 @@ class DetailPage extends StatelessWidget {
   SplayTreeMap<DateTime, List<Task>> getTaskMap() {
     SplayTreeMap<DateTime, List<Task>> map = SplayTreeMap();
 
-    for (Task task in tasks) {
+    for (Task task in widget.tasks) {
       if (map[task.selectedDate.startDate] == null) {
         map[task.selectedDate.startDate] = List.empty(growable: true);
       }
